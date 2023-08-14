4bc8c10bebb7b8e78ff82e54db76c07eb16ea651
ShoonLei98 <106812215+ShoonLei98@users.noreply.github.com>
2023-08-14 10:23:57 +0630

    july monthly all frames certificate

--- src/BonusFrame.jsx
+++ src/BonusFrame.jsx
@@ -1,29 +1,31 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import './assets/css/_july_certificate_card.scss';
-import AllFrameCertificate from "./feature/may_bonus_frames/AllFrameCertificate";
-import BonusReportColor from './data/may/BonusReportColor.json';
+import AllFrameCertificate from "./feature/may_bonus_frames/JulyAllFrameCertificate";
+import BonusReportColor from './data/may/JulyBonusReportColor.json';
 
 export default function BonusFrame(){
 
     const [bonus, setBonus] = useState();
     const [loading, setLoading] = useState(false); 
     const [error, setError] = useState(false); 
-    const token = '104683|zGElkZT2kNC7rcCyvuFhrUg5NzMDVZmiimiJido3';
+    const token = '105851|375rbU7dI3PF3LD4a2at5m4rrezSvJBSS5YyobUx';
     const bonusReportColor = BonusReportColor.find(color => (color.product_id === 0));
 
     useEffect(() => {
         getBonusFrame()
     },[])
-
+    const region = 'MM'
+    const monthlyURL = 'https://api.dev.focusbeauty.net/api/admin/bonus-awards/all'
+    const sixMonthURL = 'https://api.dev.focusbeauty.net/api/admin/highest-bonus-awards/all'
     const getBonusFrame = async () => {
         try {
             setLoading(true)
-            const response = await axios.post('https://api.dev.focusbeauty.net/api/admin/bonus-awards/all',
+            const response = await axios.post(`${sixMonthURL}`,
             {
-                sale_region : 'MM',
+                sale_region : region,
                 bonus_date : '2023-06-01',
-                agent_code : 'FB-000695'
+                // agent_code : 'FB-000005'
             }, 
             {
                 headers : {
@@ -42,7 +44,7 @@ export default function BonusFrame(){
     return (
         <div className="report_card_list_container">
           { loading ? "Loading..." : bonus?.map((bonus) =>(
-            <AllFrameCertificate key={bonus?.id} info={bonus} color={bonusReportColor} region={'MM'}/>
+            <AllFrameCertificate key={bonus?.id} info={bonus} color={bonusReportColor} region={region}/>
           ) 
           )}
 
--- src/feature/may_bonus_frames/JulyAllFrameCertificate.jsx
+++ src/feature/may_bonus_frames/JulyAllFrameCertificate.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Umbrella from '../../data/image/umbrella.png'
 // import { getBase64FromUrl } from '../../utils/index'
 // import allFrameImg from '../../assets/images/frames/all_frame.png'
 // import defaultAllFrame from '../../assets/images/defaultFrameProfile.jpg'
@@ -13,10 +12,10 @@ const AllFrameReport = (props) => {
   const agent_name_length = info?.agent_name?.length
   const name_size =
     agent_name_length <= 18
-      ? '140px'
+      ? '190px'
       : (agent_name_length > 18) & (agent_name_length <= 22)
-      ? '135px'
-      : '128px'
+      ? '175px'
+      : '155px'
 
   return (
     <>      
@@ -28,125 +27,68 @@ const AllFrameReport = (props) => {
           className="report-frame"
         />
         <div className="bonus-report-info-container">
-          <div className="bonus-month months">{info?.bonus_month?.split(' ')[1]}</div>
-          <div className="bonus-month year">{info?.bonus_month?.split(' ')[0]}</div>
-          <img src={Umbrella} alt="Umbrella" className="bonus-month-img"></img>
-          <h1 className="bonus-level" style={{ color: color['level_color'] }}>
+            <span className="bonus-month months">{info?.bonus_month?.split(' ')[1]}</span>
+            <span className="bonus-month year">{info?.bonus_month?.split(' ')[0]}</span>
+            <h1 className="bonus-level" style={{ color: color['level_color'] }}>
             {info?.max_bonus_level}
-          </h1>
-          <div className={info?.bonus_products?.length >= 10 ? "agent-much-bonus-info-container" : "agent-bonus-info-container"}>
+            </h1>
             <div className="agent-profile-container">
-              <img src={info?.profile} alt="profile" className="agent-profile" />
+                <img src={info?.profile} alt="profile" className="agent-profile" />
             </div>
             <h1 className="agent-name" style={{ fontSize: name_size, color: color['agent_name'] }}>
-              {info?.agent_name}
+                {info?.agent_name}
             </h1>
             <span
-              className="agent-code prefix-name"
-              style={{
+                className="agent-code prefix-name"
+                style={{
                 color: color['agent_code'],
-              }}
+                }}
             >
-              FB
+                FB
             </span>
             <span
-              className="agent-code"
-              style={{
+                className="agent-code"
+                style={{
                 color: color['agent_code'],
-              }}
+                }}
             >
-              {info?.agent_code?.split('-')[1]}
+                {info?.agent_code?.split('-')[1]}
             </span>
             <div
-              className="award-amount"
-              style={{ color: color['award_amount'] }}
+                className="award-amount"
+                style={{ color: color['award_amount'] }}
             >
-              <div className="amount_name">ဆုကြေးငွေ&nbsp;-&nbsp;</div>
-              <div className="amount_value">
-                {region === 'MM' ? info?.total_award_amount_mmk : info?.total_award_amount}{' '}
+                <div className="amount_name">ဆုကြေးငွေ&nbsp;-&nbsp;</div>
+                <div className="amount_value">
+                {info?.total_award_amount}{' '}
                 {region === 'MM' ? 'ကျပ်' : 'THB'}
-              </div>
+                </div>
             </div>
             <div
-              className="bonus-product-list"
-              style={{
+                className="bonus-product-list"
+                style={{
                 rowGap:
-                  info?.bonus_products?.length >= 10
-                    ? '45px'
-                    : info?.bonus_products?.length >= 8
+                    info?.bonus_products?.length >= 10
                     ? '25px'
-                    : '40px',
-              }}
+                    : info?.bonus_products?.length >= 8
+                    ? '43px'
+                    : '65px',
+                }}
             >
-              {info?.bonus_products?.length > 0
+                {info?.bonus_products?.length > 0
                 ? info?.bonus_products?.map((info, index) => (
                     <FrameProductInfo
-                      productInfo={info}
-                      key={index}
-                      rowCount={info?.bonus_products?.length ?? 0}
-                      colorInfo={{
+                        productInfo={info}
+                        key={index}
+                        rowCount={info?.bonus_products?.length ?? 0}
+                        colorInfo={{
                         name: color['product_name'],
                         value: color['product_qty'],
-                      }}
+                        }}
                     />
-                  ))
+                    ))
                 : null}
             </div>
-          </div>
-          {/* {info?.bonus_products?.length >= 10 ? (
-            
-          ) : (
-            <div className='bonus-report-card-container'>
-              <div className="agent-profile-container">
-                <img src={info?.profile} alt="profile" className="agent-profile" />
-              </div>
-              <div className="bonus-report-info-container">
-                <h1 className="agent-name" style={{ color: color['agent_name'] }}>
-                  {info?.agent_name}
-                </h1>
-                <span className="agent-code prefix-name" style={{ color: color['agent_code'] }}>
-                  FB
-                </span>
-                <span className="agent-code" style={{ color: color['agent_code'] }}>
-                  {info?.agent_code?.split('-')[1]}
-                </span>
-                <div className="award-amount" style={{ color: color['award_amount'] }}>
-                  <div className="amount_name">ဆုကြေးငွေ&nbsp;-&nbsp;</div>
-                  <div className="amount_value">
-                    {region === 'MM' ? info?.total_award_amount_mmk : info?.total_award_amount}{' '}
-                    {region === 'MM' ? 'ကျပ်' : 'THB'}
-                  </div>
-                </div>
-                <div
-                  className="bonus-product-list"
-                  style={{
-                    rowGap:
-                      info?.bonus_products?.length >= 10
-                        ? '45px'
-                        : info?.bonus_products?.length >= 8
-                        ? '25px'
-                        : '40px',
-                    top: info?.bonus_products?.length >= 10 ? '69%' : '82.5%',
-                  }}
-                >
-                  {info?.bonus_products?.length > 0
-                    ? info?.bonus_products?.map((info, index) => (
-                        <FrameProductInfo
-                          productInfo={info}
-                          key={index}
-                          rowCount={info?.bonus_products?.length ?? 0}
-                          colorInfo={{
-                            name: color['product_name'],
-                            value: color['product_qty'],
-                          }}
-                        />
-                      ))
-                    : null}
-                </div>
-              </div>
-            </div>
-            
-          )} */}
         </div>
       </div>        
     </>
@@ -155,7 +97,7 @@ const AllFrameReport = (props) => {
 
 const FrameProductInfo = ({ productInfo, colorInfo, rowCount }) => {
   return (
-    <div className="product-info-row" style={{ fontSize: rowCount >= 10 ? '40px' : '50px' }}>
+    <div className="product-info-row" style={{ fontSize: rowCount >= 10 ? '35px' : '55px' }}>
       <span className="product-name" style={{ color: colorInfo['name'] }}>
         {productInfo?.name}&nbsp;:&nbsp;
       </span>
